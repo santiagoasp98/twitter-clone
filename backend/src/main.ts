bd36a9676c746d0ac410168a6cd99011500e2445
Santiago <santiago.asp.98@gmail.com>
2024-02-03 09:23:34 -0300

    add www

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,8 @@ import { AppModule } from './app.module'
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.enableCors({
-        origin: 'https://twitter-clone-frontend-ecru.vercel.app', // or http://localhost:5173
+        origin: 'https://www.twitter-clone-frontend-ecru.vercel.app', // or http://localhost:5173
         credentials: true,
-        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-        methods: 'GET, POST, PUT, DELETE, OPTIONS',
     })
     await app.listen(process.env.PORT)
 }
