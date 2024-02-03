7dd596ec3d2e44038d07e85828805e5b2210bfe0
Santiago <santiago.asp.98@gmail.com>
2024-02-03 09:12:02 -0300

    update cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.enableCors({
-        origin: 'https://twitter-clone-frontend-ecru.vercel.app', // or http://localhost:5173
+        origin: 'https://twitter-clone-frontend-ecru.vercel.app/', // or http://localhost:5173
         credentials: true,
     })
     await app.listen(process.env.PORT)
